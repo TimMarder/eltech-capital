595b4b8b999dbf5f6787a50a3a88642554d3921b
Tim Marder <timmarder@gmail.com>
2026-02-26 06:16:22 +0000

    Tighten footer logo spacing - remove padding above and below logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,8 +8,8 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {/* About */}
-          <div className="text-left">
-            <div className="relative h-[519px] w-[291px] mb-2">
+          <div className="text-left -mt-4">
+            <div className="relative h-[519px] w-[291px] -mb-8">
               <Image
                 src="/images/CLEAR Horizontal Banner without margins.png"
                 alt="ELTECH Capital"
